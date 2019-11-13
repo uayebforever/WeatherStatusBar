37c0a78c1e05f89803bdcea8612eb4a24de73806
Andy Green <uayebforever@gmail.com>
2019-11-13 20:31:22 +1100

    Uncommited changes—unknown

---
+++ WeatherStatusBar/wsbStation.h
@@ -0,0 +1,13 @@
+//
+//  wsbStation.h
+//  WeatherStatusBar
+//
+//  Created by Andy Green on 2/02/13.
+//  Copyright (c) 2013 Andy Green. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface wsbStation : NSObject
+
+@end
---
+++ WeatherStatusBar/wsbStation.m
@@ -0,0 +1,13 @@
+//
+//  wsbStation.m
+//  WeatherStatusBar
+//
+//  Created by Andy Green on 2/02/13.
+//  Copyright (c) 2013 Andy Green. All rights reserved.
+//
+
+#import "wsbStation.h"
+
+@implementation wsbStation
+
+@end
